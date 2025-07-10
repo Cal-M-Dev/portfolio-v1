c4661309983944330d24bf4885cead254fb01a42
Cal M <cal.mc.dev@gmail.com>
2025-07-10 11:39:03 +0100

    V1.2.2

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
 console.log("V1 script loaded!");
+
+document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('section[id]');
+    const navLinks = document.querySelectorAll('.nav-links a');
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            const id = entry.target.getAttribute('id');
+            const link = document.querySelector(`.nav-links a[href="#${id}"]`);
+
+            if (entry.isIntersecting) {
+                link.parentElement.classList.add('active');
+            } else {
+                link.parentElement.classList.remove('active');
+            }
+        });
+    },      {
+        rootMargin: '-50% 0px -50% 0px'
+    });
+
+    sections.forEach(section => observer.observe(section));
+});
