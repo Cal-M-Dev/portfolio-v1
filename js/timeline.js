2956e5a85509de03cde4d5b19e9669cbbb967750
Cal M <cal.mc.dev@gmail.com>
2025-07-18 19:18:58 +0100

    1.5 First Draft on Timeline section

--- js/timeline.js
+++ js/timeline.js
@@ -1,5 +1,19 @@
+console.log("Timeline Loaded");
 const timelineEvents = [
     {date: "06/2025", title: "Started CS50x", description: "Intro to computer science, multiple beginner projects using C, Python, Javascript, CSS, HTML, SQL"},
     {date: "07/2025", title: "Finished CS50x Final Project", description: "Made V1 version of my portfolio website using Javascript, CSS, HTML"},
     {date: "07/2025", title: "Started CS50 Python", description: "Building Python fluency"}   
 ]
+
+const eventsList = document.querySelector(".events");
+
+timelineEvents.forEach(event => {
+    const li = document.createElement("li");
+
+    li.innerHTML = `
+        <h4>${event.date} - ${event.title}</h4>
+        <p>${event.description}</p>
+    `;
+
+    eventsList.appendChild(li);
+});
