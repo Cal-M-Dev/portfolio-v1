348ed8a6d0cbf4b3016c506b06d8681d512ae109
Cal M <cal.mc.dev@gmail.com>
2025-08-01 12:21:28 +0100

    V1.8 Timeline Final Version

--- js/timeline.js
+++ js/timeline.js
@@ -5,7 +5,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const timelineEvents = [
         {date: "06/2025", title: "Started CS50x", description: "Intro to computer science, multiple beginner projects using C, Python, Javascript, CSS, HTML, SQL"},
         {date: "07/2025", title: "Finished CS50x Final Project", description: "Made V1 version of my portfolio website using Javascript, CSS, HTML"},
-        {date: "07/2025", title: "Started CS50 Python", description: "Building Python fluency"}   
+        {date: "07/2025", title: "Started CS50 Python", description: "Building Python fluency"},
+        {date: "PLACEHOLDER", title: "PLACEHOLDER", description: "PLACEHOLDER"},
+        {date: "PLACEHOLDER", title: "PLACEHOLDER", description: "PLACEHOLDER"},
+        {date: "PLACEHOLDER", title: "PLACEHOLDER", description: "PLACEHOLDER"}         
     ];
 
     const eventsList = document.querySelector(".events");
